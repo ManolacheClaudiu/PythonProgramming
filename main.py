b6eaa8a727abe57575d8ef691b96bf62f122eb99
ManolacheClaudiu <79422343+ManolacheClaudiu@users.noreply.github.com>
2022-01-02 04:06:02 +0200

    Add files via upload
    - conectarea la o baza de date
    - citirea din facturi de tip json

--- main.py
+++ main.py
@@ -5,6 +5,9 @@
 
 import mysql.connector
 from mysql.connector import Error
+import os, json
+import pandas as pd
+
 
 
 def create_connection(host_name, user_name, user_password):
@@ -36,3 +39,26 @@ if __name__ == '__main__':
     connection = create_connection("localhost", "root", "12345678")
     create_database_query = "CREATE DATABASE ExpenseAlert"
     create_database(connection, create_database_query)
+
+    path_to_json = 'facturi'
+    json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+    jsons_data = pd.DataFrame(columns=["price", "category", "number", "month", "year", "day"])
+    for index, js in enumerate(json_files):
+        with open(os.path.join(path_to_json, js)) as json_file:
+            json_text = json.load(json_file)
+
+            # here you need to know the layout of your json and each json has to have
+            # the same structure (obviously not the structure I have here)
+            price= json_text['price']
+            category = json_text['category']
+            number = json_text['number']
+            month = json_text['month']
+            year = json_text['year']
+            day = json_text['day']
+
+            # here I push a list of data into a pandas DataFrame at row given by 'index'
+            jsons_data.loc[index] = [price, category, number, month, year, day]
+
+    # now that we have the pertinent json data in our DataFrame let's look at it
+    print(jsons_data)
+
